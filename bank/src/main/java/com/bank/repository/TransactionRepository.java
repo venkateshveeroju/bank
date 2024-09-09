8d63ba084db236d2dfa6280d0d48313914ceb4b0
venkateshveeroju <veerojuvenkatesh5@gmail.com>
2024-09-09 18:11:22 +0200

    updated transaction repository file
    updated transaction repository file

--- bank/src/main/java/com/bank/repository/TransactionRepository.java
+++ bank/src/main/java/com/bank/repository/TransactionRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    Transaction findByUserid(Long userId);
-    Transaction findByid(Long id);
+
 }
